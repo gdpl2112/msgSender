7b1d88ef1bb2eeef45ed274f19699785ade25442
HRS-Computer <3474006766@qq.com>
2023-03-15 10:29:55 +0800

    陌生人/群临时会话消息支持转发

--- src/main/java/io/github/gdpl2112/msgSender/DefaultListenerHost.java
+++ src/main/java/io/github/gdpl2112/msgSender/DefaultListenerHost.java
@@ -1,11 +1,12 @@
 package io.github.gdpl2112.msgSender;
 
 import kotlin.coroutines.CoroutineContext;
+import net.mamoe.mirai.Bot;
 import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.SimpleListenerHost;
-import net.mamoe.mirai.event.events.FriendMessageEvent;
-import net.mamoe.mirai.event.events.GroupMessageEvent;
+import net.mamoe.mirai.event.events.*;
 import net.mamoe.mirai.message.data.At;
+import net.mamoe.mirai.message.data.MessageChain;
 import net.mamoe.mirai.message.data.SingleMessage;
 import org.jetbrains.annotations.NotNull;
 
@@ -29,6 +30,28 @@ public class DefaultListenerHost extends SimpleListenerHost {
             );
             event.getBot().getFriend(qid).sendMessage(event.getMessage());
         }
+    }
+
+    @EventHandler
+    public void onEvent(StrangerMessageEvent event) {
+        method9(event.getSender().getId(), event.getBot(), event.getSender().getRemark(), event.getMessage(), event);
+    }
+
+    private void method9(long id, Bot bot, String remark, MessageChain message, MessageEvent event) {
+        if (id == bot.getId()) return;
+        Config config = MsgSender.INSTANCE.config;
+        long qid = config.getQid();
+        if (qid > 0) {
+            bot.getFriend(qid).sendMessage(
+                    String.format(config.getTips(), remark, id)
+            );
+            bot.getFriend(qid).sendMessage(message);
+        }
+    }
+
+    @EventHandler
+    public void onEvent(GroupTempMessageEvent event) {
+        method9(event.getSender().getId(), event.getBot(), event.getSender().getRemark(), event.getMessage(), event);
     }
 
     @EventHandler
